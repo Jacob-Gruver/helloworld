f8c64dc7ff00b5d66205389e8cbb324a6385bb85
Jacob-Gruver <gruvinbro@gmail.com>
2021-05-10 15:46:00 -0600

    init

--- Program.cs
+++ Program.cs
@@ -8,24 +8,10 @@ namespace helloworld
         {
             Console.WriteLine("Hello Dude-perfect!");
 
-            Console.WriteLine("Check out my equations! Try the Pythagorean Theorem using 23 and 45!");
-
-            var pow = 2;
-
-            var x = 23;
-
-            var y = 45;
-
-            var xpow = Math.Pow(x, pow);
-
-            var ypow = Math.Pow(y, pow);
-
-            var addedPow = xpow + ypow;
-
-            var pythTh = Math.Pow(addedPow, pow);
-
-            Console.WriteLine(pythTh);
-
+            if (true)
+            {
+                System.Console.WriteLine("this statement is true");
+            }
 
         }
     }
